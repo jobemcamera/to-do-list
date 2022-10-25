3b660162c39f7988018e11094abe45a7a99e2a18
jobemcamera <ti.jobecamera@gmail.com>
2022-10-25 19:23:30 -0300

    divisão html melhorada; estilização do css reformulada; script vou recomeçar

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
-const botaoAdd = document.querySelector("#btn-add")
-const botaoDel = document.querySelector("#btn-remove")
+const novaTarefa = document.querySelector(".nova-tarefa-input")
+const novaTarefaBtn = document.querySelector(".nova-tarefa-botao")
 
-
-botaoAdd.addEventListener("click", mostraTarefas)
-botaoDel.addEventListener("click", deletaTarefas)
-
-
-function mostraTarefas() {
-
-	if (document.getElementById("texto-tarefa").value.length == 0) {
-		return 0;
+function validarInput () {
+	if (novaTarefa.value.trim().length > 0) {
+		return true;
 	} else {
-		document.getElementById("mostra-tarefas").innerHTML += `
-		<input type="checkbox">
-            <ul id="lista-tarefas">
-                 <li>${document.getElementById("texto-tarefa").value}</li>
-            </ul>
-
-        <button id="btn-remove">Remover</button>
-        `;
+		return false;
 	}
 }
 
+novaTarefaBtn.addEventListener("click", addTarefa)
 
-
-function deletaTarefas() {
-
-    alert("deletado")
-
+function addTarefa() {
 
 }
-
