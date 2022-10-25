a779cb30eee30717e0786af904b8ea37f23c947e
jobemcamera <ti.jobecamera@gmail.com>
2022-10-25 20:11:56 -0300

    função do botão adicionar tarefa feita

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const novaTarefa = document.querySelector(".nova-tarefa-input")
 const novaTarefaBtn = document.querySelector(".nova-tarefa-botao")
+const tarefaContainer = document.querySelector(".tarefas-container")
 
 function validarInput () {
 	if (novaTarefa.value.trim().length > 0) {
@@ -12,5 +13,18 @@ function validarInput () {
 novaTarefaBtn.addEventListener("click", addTarefa)
 
 function addTarefa() {
+    // criando uma tag div no html
+    const tarefaItemContainer = document.createElement('div') 
+    // adicionando uma class na div
+    tarefaItemContainer.classList.add('tarefa-item') 
+    // criando uma tag p na div
+    const tarefaConteudo = document.createElement('p')  
+    // inserindo a tarefa na tag p
+    tarefaConteudo.innerText = novaTarefa.value
 
+    tarefaItemContainer.appendChild(tarefaConteudo)
+
+    tarefaContainer.appendChild(tarefaItemContainer)
+
+    tarefaContainer.style.display = 'flex'
 }
