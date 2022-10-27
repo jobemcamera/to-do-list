e26f52eb451958f1a2e5bd52d91794ddd71bae66
jobemcamera <ti.jobecamera@gmail.com>
2022-10-26 21:58:15 -0300

    mudança na estilização do checkbox e identificador do ícone de apagar

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const novaTarefa = document.querySelector(".nova-tarefa-input")
 const novaTarefaBtn = document.querySelector(".nova-tarefa-botao")
 const tarefaContainer = document.querySelector(".tarefas-container")
+const deletaTarefaBtn = document.querySelector("#lixo")
 
+// função que verifica o campo input vazio incluindo somente espaços
 function validarInput () {
 	if (novaTarefa.value.trim().length > 0) {
 		return true;
@@ -15,24 +17,31 @@ novaTarefaBtn.addEventListener("click", addTarefa)
 function addTarefa() {
     // criando uma tag div no html
     const tarefaItemContainer = document.createElement('div') 
+
     // adicionando uma class na div
     tarefaItemContainer.classList.add('tarefa-item') 
+
     // criando uma tag p na div
     const tarefaConteudo = document.createElement('p')  
+
     // criando uma tag input
     const tarefaCheck = document.createElement('input')
+
     // criando o type checkbox da tag input
     tarefaCheck.setAttribute("type", "checkbox")
+
     // inserindo a tarefa na tag p
     tarefaConteudo.innerText = novaTarefa.value
+
     // criando a tag i com o icone de lixeira
-    const deletatarefa = document.createElement("i")
-    deletatarefa.classList.add("fa-solid")
-    deletatarefa.classList.add("fa-trash")
+    const deletaTarefa = document.createElement("i")
+    deletaTarefa.classList.add("fa-solid")
+    deletaTarefa.classList.add("fa-trash")
+    deletaTarefa.setAttribute("id", "lixo")
 
     tarefaItemContainer.appendChild(tarefaCheck)
     tarefaItemContainer.appendChild(tarefaConteudo)
-    tarefaItemContainer.appendChild(deletatarefa)
+    tarefaItemContainer.appendChild(deletaTarefa)
     tarefaContainer.appendChild(tarefaItemContainer)
 
     // reexibe o container já criado no html para guardar as tarefas digitas
